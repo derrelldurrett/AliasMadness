9c5d1faf7c8ac53cc9f720830dc45e53ce90b3cf
derrelldurrett <derrelldurrett+github@gmail.com>
2015-03-19 20:59:47 -0600

    fix message sending

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,17 +1,36 @@
 class Admin
   class MessagesController < ApplicationController
     include SessionsHelper
+    include UsersHelper
+    include MailHelper
+
     before_filter :check_authorization
-    MESSAGE_KEYS=[:subject, :message]
+    MESSAGE_KEYS=[:subject, :message, :to]
 
     def new
       @user= Admin.get
     end
 
     def create
-      MessageMailer.message_mail(params.slice(*MESSAGE_KEYS)).deliver
+      params[:message]= CGI::escapeHTML(params[:message]).gsub(/\n/, '<br>').html_safe
+      get_players.each_slice(20) do |p_slice|
+        params[:to]= build_players_email_list p_slice
+        MessageMailer.message_mail(params.slice(*MESSAGE_KEYS)).deliver
+      end
       @user=Admin.get
       render new_admin_message_path and return
     end
+
+    private
+
+    def build_players_email_list(players)
+      list=[]
+      players.each do |p|
+        list<< construct_player_email_to_field(p)
+      end
+      list.join(',')
+    end
+
   end
+
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,22 +1,8 @@
 class MessageMailer < ActionMailer::Base
-  include MailHelper
-  include UsersHelper
-
   def message_mail(args)
     @subject= args[:subject]
-    @message= CGI::escapeHTML(args[:message]).gsub(/\n/, '<br>').html_safe
-    get_players.each_slice(20) do |p_slice|
-      mail(from: ENV['ALIASMADNESS_SERVEREMAIL'], to: build_players_email_list(p_slice), subject: @subject)
-    end
+    @message=
+        mail(from: ENV['ALIASMADNESS_SERVEREMAIL'], to: args[:to], subject: @subject)
   end
 
-  private
-
-  def build_players_email_list(players)
-    list=[]
-    players.each do |p|
-      list<< construct_player_email_to_field(p)
-    end
-    list.join(',')
-  end
 end
