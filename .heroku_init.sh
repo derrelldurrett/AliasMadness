04b573a853a5fd1f10de8733a810e63e5077facd
derrelldurrett <derrelldurrett+github@gmail.com>
2019-03-18 11:07:32 -0600

    final (?) steps to get it working, including removing a lot of cruft.

--- .heroku_init.sh
+++ .heroku_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(while read VAR ; do heroku config:set --app alias-madness-heroku-18 $VAR; done ) < .heroku_env
-heroku pg:reset --app alias-madness-heroku-18 aliasmadness_production
-heroku run --app alias-madness-heroku-18 rake db:migrate db:seed
+(while read VAR ; do heroku config:set --remote $1 $VAR; done ) < .heroku_env
+heroku pg:reset -r $1
+heroku run -r $1 rake db:migrate db:seed
