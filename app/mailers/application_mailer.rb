e8ced801dadde3779626a7a6d8d0fb13b488a3d2
derrelldurrett <derrelldurrett+github@gmail.com>
2021-03-15 13:54:35 -0600

    Succeeded so far -- try on heroku next

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV['ALIASMADNESS_SERVEREMAIL'],
-          reply_to: ENV['ALIASMADNESS_ADMINEMAIL'],
-          headers: {'Authorization' => 'Bearer ' + ENV['ALIASMADNESS_SENDGRID_API_KEY']}
+          reply_to: ENV['ALIASMADNESS_ADMINEMAIL'] #,
+  #          headers: {'Authorization' => 'Bearer ' + ENV['ALIASMADNESS_SENDGRID_API_KEY']}
   layout 'mailer'
 end
