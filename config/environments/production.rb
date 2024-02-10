568fae309285a6e93b730a108fc73a2536745baa
derrelldurrett <derrelldurrett+github@gmail.com>
2024-02-09 17:35:37 -0700

    config.host_authorization....

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << ENV['ALIASMADNESS_HOST']
-  config.host_authorization = ENV['ALIASMADNESS_HOST']
+  config.hosts << ENV['ALIASMADNESS_HOSTNAME']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
